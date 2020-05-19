07b6fef2c0c424a45f0a17e7740c774c84634eac
tryingpfq <tryingpfq@163.com>
2020-05-19 16:02:20 +0800

    feat(代理):代理

--- src/main/java/com/tryingpfq/proxy/cglib/Main.java
+++ src/main/java/com/tryingpfq/proxy/cglib/Main.java
@@ -1,5 +1,8 @@
 package com.tryingpfq.proxy.cglib;
 
+import net.sf.cglib.core.DebuggingClassWriter;
+import net.sf.cglib.proxy.Enhancer;
+
 import java.io.InputStreamReader;
 
 /**
@@ -8,18 +11,18 @@ import java.io.InputStreamReader;
  */
 public class Main {
     public static void main(String[] args) {
-       // System.setProperty(DebuggingClassWriter.DEBUG_LOCATION_PROPERTY,"out\\proxy");
+        System.setProperty(DebuggingClassWriter.DEBUG_LOCATION_PROPERTY,"out\\proxy\\cglib");
 
         //通过CGLib动态代理获取代理对象的过程
-//        Enhancer enhancer = new Enhancer();
-//        //设置enhancer的父类
-//        enhancer.setSuperclass(TeamService.class);
-//        //设置回调对象
-//        enhancer.setCallback(new MyMethodInterceptor());
-//        //创建代理对象
-//        TeamService proxy = (TeamService) enhancer.create();
-//        //proxy.accept(123456L);
-//        proxy.apply();
+        Enhancer enhancer = new Enhancer();
+        //设置enhancer的父类
+        enhancer.setSuperclass(TeamService.class);
+        //设置回调对象
+        enhancer.setCallback(new MyMethodInterceptor());
+        //创建代理对象
+        TeamService proxy = (TeamService) enhancer.create();
+        //proxy.accept(123456L);
+        proxy.apply();
 
         TeamService teamService = new TeamService();
         System.out.println(teamService.getClass().getClassLoader().getResource("ideacode.txt"));
