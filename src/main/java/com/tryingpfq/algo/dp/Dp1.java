4cdfd2e6dddcbb5e29f760ab21ef3901bc9201ef
tryingpfq <tryingpfq@163.com>
2020-07-18 16:45:33 +0800

    feat(algo):交错字符串

--- src/main/java/com/tryingpfq/algo/dp/Dp1.java
+++ src/main/java/com/tryingpfq/algo/dp/Dp1.java
@@ -7,7 +7,7 @@ package com.tryingpfq.algo.dp;
 public class Dp1 {
 
     public static int treesCount(int n) {
-        int[] f = new int[n+1];
+        int[] f = new int[n + 1];
 
         f[0] = 1;
         f[1] = 1;
@@ -20,7 +20,41 @@ public class Dp1 {
         return f[n];
     }
 
+
+    /**
+     * 交错字符串 leetcode[97]
+     *
+     * @param s1
+     * @param s2
+     * @param s3
+     * @return
+     */
+    public static boolean isInterleave(String s1, String s2, String s3) {
+        int l1 = s1.length(), l2 = s2.length(), l3 = s3.length();
+
+        if (l1 + l2 != l3) {
+            return false;
+        }
+
+        boolean[][] f = new boolean[l1 + 1][l2 + 1];
+        f[0][0] = true;
+        for (int i = 0; i <= l1; i++) {
+            for (int j = 0; j <= l2; j++) {
+                int p = i + j - 1;
+                if (i > 0) {
+                    f[i][j] |= (f[i-1][j] && s1.charAt(i-1) ==  s3.charAt(p));
+                }
+                if (j > 0) {
+                    f[i][j] |= (f[i][j-1] && s2.charAt(j-1) ==  s3.charAt(p));
+                }
+            }
+        }
+
+        return f[l1][l2];
+    }
+
+
     public static void main(String[] args) {
-        System.err.println(treesCount(17));
+        System.out.println(isInterleave("aabcc","dbbca","aadbbcbcac"));
     }
 }
