e3b66a4c5c907a6b0818f30b793b1f1e7f4fa842
tryingpfq <tryingpfq@163.com>
2020-07-23 20:39:32 +0800

    feat(algo):LinkLRU

--- src/main/java/com/tryingpfq/algo/leetcode/Solution.java
+++ src/main/java/com/tryingpfq/algo/leetcode/Solution.java
@@ -116,4 +116,31 @@ public class Solution {
         }
     }
 
+    /**
+     * 最小路劲 动态规划 64
+     * @param grid
+     * @return
+     */
+    public int minPathSum(int[][] grid){
+        if (grid == null || grid.length == 0 || grid[0].length == 0) {
+            return -1;
+        }
+        int row = grid.length,columns = grid[0].length;
+        int[][] dp = new int[row][columns];
+        dp[0][0] = grid[0][0];
+        for (int i = 1; i < row; i++) {
+            dp[i][0] = dp[i - 1][0] + grid[i][0];
+        }
+        for (int j = 1; j < columns; j++) {
+            dp[0][j] = dp[0][j - 1] + grid[0][j];
+        }
+
+        for (int i = 1; i < row; i++) {
+            for (int j = 1; j < columns; j++) {
+                dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+            }
+        }
+        return dp[row - 1][columns - 1];
+    }
+
 }
