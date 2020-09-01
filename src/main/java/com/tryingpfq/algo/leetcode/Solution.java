3055cff5733df04ef3a77df8d9e6b7591f696015
tryingpfq <tryingpfq@163.com>
2020-09-01 16:28:02 +0800

    feat(algo):leetcode

--- src/main/java/com/tryingpfq/algo/leetcode/Solution.java
+++ src/main/java/com/tryingpfq/algo/leetcode/Solution.java
@@ -753,4 +753,64 @@ public class Solution {
         }
         return true;
     }
+
+    /**
+     * 33 搜索旋转有序数组
+     *
+     * @param nums   数组
+     * @param target 目标
+     * @return
+     */
+    public int search(int[] nums, int target) {
+        int n = nums.length;
+        if (n <= 0) {
+            return -1;
+        }
+        if (n == 1) return nums[0] == target ? 0 : -1;
+        int l = 0, r = n - 1;
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            if (nums[mid] == target) return mid;
+            if (nums[0] <= nums[mid]) { // 这个区间不存在旋转
+                if (nums[0] <= target && target < nums[mid]) {
+                    r = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[n - 1]) {
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 841 钥匙和房间
+     * @param rooms
+     * @return
+     */
+    boolean[] vis;
+    int num;
+
+    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        int n = rooms.size();
+        num = 0;
+        vis = new boolean[n];
+        dfs(rooms, 0);
+        return num == n;
+    }
+
+    public void dfs(List<List<Integer>> rooms, int x) {
+        vis[x] = true;
+        num++;
+        for (int i : rooms.get(x)) {
+            if (!vis[i]) {
+                dfs(rooms, i);
+            }
+        }
+    }
 }
